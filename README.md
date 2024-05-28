088aff39972cdc89444e361329fe97f365204074
Thomas Fel <thomas_fel@brown.edu>
2024-05-28 19:43:41 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,28 @@
+# Torqueo: A Compact Library for Image Warping in Pytorch
+
+Todo.
+
+# 🚀 Getting Started with Torqueo
+
+Torqueo requires Python 3.6 or newer and several dependencies, including Numpy. Installation is straightforward with Pypi:
+
+```bash
+pip install torqueo
+```
+
+With Torqueo installed, you can dive into image warping. The API is designed to be intuitive, requiring only a few hyperparameters to get started.
+
+Example usage:
+
+```python
+import torch
+import timm
+from torqueo import XYZ
+
+@todo
+```
+
+# Authors of the code
+
+- [Thomas Fel](https://thomasfel.fr) - thomas_fel@brown.edu, PhD Student DEEL (ANITI), Brown University
+- [Vipul Sharma]() - vipul_sharma@brown.edu, Brown University
---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+with open("README.md", encoding="utf-8") as fh:
+    README = fh.read()
+
+setup(
+    name="Torqueo",
+    version="0.0.0",
+    description="Personal toolbox for image Warping",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    author="Thomas Fel, Vipul Sharma",
+    author_email="thomas_fel@brown.edu",
+    license="MIT",
+    install_requires=['numpy','matplotlib', 'torch', 'torchvision', 'Pillow'],
+    packages=find_packages(),
+    python_requires=">=3.6",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+)

c22a11fb3a416158f9c1e5219138f0d878cb3c2f
vipulSharma18 <vipuls181999@gmail.com>
2024-05-31 16:50:03 -0400

    barrel, pincushion, compression and stretch done

--- torqueo/transforms.py
+++ torqueo/transforms.py
@@ -3,6 +3,16 @@ import torch.nn.functional as F
 
 from .base import WarpTransform
 
+'''
+Assumptions: 
+
+1. The current version of the code assumes "centrality", i.e., the object of interest
+is at the center of the image. A more accurate version would be one that uses 
+ImageNet bounding boxes or clickMe maps to determine the center for warping on per image basis. 
+
+'''
+
+
 class Fisheye(WarpTransform):
     def __init__(self, strength=0.5):
         super(Fisheye, self).__init__()
@@ -24,3 +34,117 @@ class Fisheye(WarpTransform):
         grid = torch.stack((x_new, y_new), dim=-1)
         grid = grid.unsqueeze(0)
         return grid
+
+class BarrelDistortion(WarpTransform):
+    """
+    Implemented based on the single-term division model that is
+    "sufficient" for most cameras but not "complete" warping equation. 
+    https://en.wikipedia.org/wiki/Distortion_(optics)
+    """
+    def __init__(self, strength=0.5):
+        super(BarrelDistortion, self).__init__()
+        self.strength = strength
+
+    def generate_warp_field(self, height, width):
+        y_indices, x_indices = torch.meshgrid(torch.linspace(-1, 1, height), torch.linspace(-1, 1, width))
+        r = torch.sqrt(x_indices ** 2 + y_indices ** 2)
+
+        k = -1*self.strength
+        distortion = (1-torch.sqrt(1-4*k*r**2))/(2*k*r**2)
+        x_new = x_indices*distortion
+        y_new = y_indices*distortion
+
+        x_new = x_new.clamp(-1, 1)
+        y_new = y_new.clamp(-1, 1)
+
+        grid = torch.stack((x_new, y_new), dim=-1)
+        grid = grid.unsqueeze(0)
+        return grid
+
+class PincushionDistortion(WarpTransform):
+    """
+    Implemented based on the single-term division model that is
+    "sufficient" for most cameras but not "complete" warping equation. 
+    https://en.wikipedia.org/wiki/Distortion_(optics)
+    """
+    def __init__(self, strength=0.5):
+        super(PincushionDistortion, self).__init__()
+        self.strength = strength
+
+    def generate_warp_field(self, height, width):
+        y_indices, x_indices = torch.meshgrid(torch.linspace(-1, 1, height), torch.linspace(-1, 1, width))
+        r = torch.sqrt(x_indices ** 2 + y_indices ** 2)
+
+        k = +1*self.strength
+        distortion = (1-torch.sqrt(1-4*k*r**2))/(2*k*r**2)
+        x_new = x_indices*distortion
+        y_new = y_indices*distortion
+
+        x_new = x_new.clamp(-1, 1)
+        y_new = y_new.clamp(-1, 1)
+
+        grid = torch.stack((x_new, y_new), dim=-1)
+        grid = grid.unsqueeze(0)
+        return grid
+
+class Compression(WarpTransform):
+    '''
+    Compression/Squishing is done along width only, it's simple to extend to height but not sure if that's even desired. 
+    '''
+    def __init__(self, strength=2):
+        super(Compression, self).__init__()
+        self.strength = strength
+
+    def generate_warp_field(self, height, width):
+        y_indices, x_indices = torch.meshgrid(torch.linspace(-1, 1, height), torch.linspace(-1, 1, width))
+
+        x_new = x_indices*self.strength 
+        y_new = y_indices
+
+        x_new = x_new.clamp(-1, 1)
+        y_new = y_new.clamp(-1, 1)
+
+        grid = torch.stack((x_new, y_new), dim=-1)
+        grid = grid.unsqueeze(0)
+        return grid
+
+class Stretching(WarpTransform):
+    '''
+    Stretching is along the height dimension, not the width one. Can extend if wanted. 
+    '''
+    def __init__(self, strength=2):
+        super(Stretching, self).__init__()
+        self.strength = strength
+
+    def generate_warp_field(self, height, width):
+        y_indices, x_indices = torch.meshgrid(torch.linspace(-1, 1, height), torch.linspace(-1, 1, width))
+
+        x_new = x_indices
+        y_new = y_indices/self.strength
+
+        x_new = x_new.clamp(-1, 1)
+        y_new = y_new.clamp(-1, 1)
+
+        grid = torch.stack((x_new, y_new), dim=-1)
+        grid = grid.unsqueeze(0)
+        return grid
+
+class Twirl(WarpTransform):
+    def __init__(self, strength=2):
+        super(Twirl, self).__init__()
+        self.strength = strength
+
+    def generate_warp_field(self, height, width):
+        y_indices, x_indices = torch.meshgrid(torch.linspace(-1, 1, height), torch.linspace(-1, 1, width))
+        r = torch.sqrt(x_indices ** 2 + y_indices ** 2)
+        
+        x_new = x_indices
+        y_new = y_indices/self.strength
+
+        x_new = x_new.clamp(-1, 1)
+        y_new = y_new.clamp(-1, 1)
+
+        grid = torch.stack((x_new, y_new), dim=-1)
+        grid = grid.unsqueeze(0)
+        return grid
+

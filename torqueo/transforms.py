356531cb02b0cd2fd776fadc09d298b3d1d4f0e0
vipulSharma18 <vipuls181999@gmail.com>
2024-06-01 17:35:31 -0400

    polar warp like skimage implemented

--- torqueo/transforms.py
+++ torqueo/transforms.py
@@ -12,7 +12,6 @@ ImageNet bounding boxes or clickMe maps to determine the center for warping on p
 
 '''
 
-
 class Fisheye(WarpTransform):
     def __init__(self, strength=0.5):
         super(Fisheye, self).__init__()
@@ -242,13 +241,34 @@ class Shear(WarpTransform):
     '''
     def __init__(self, strength=0.5):
         super(Shear, self).__init__()
-        self.strength = strength
+        self.strength = strength #cotangent of the shear angle
 
     def generate_warp_field(self, height, width):
         y_indices, x_indices = torch.meshgrid(torch.linspace(-1, 1, height), torch.linspace(-1, 1, width))
 
         x_new = (1 + self.strength**2)*x_indices + self.strength*y_indices
         y_new = self.strength*x_indices + y_indices
+
+        x_new = x_new.clamp(-1, 1)
+        y_new = y_new.clamp(-1, 1)
+
+        grid = torch.stack((x_new, y_new), dim=-1)
+        grid = grid.unsqueeze(0)
+        return grid
+
+class PolarCoordinates(WarpTransform):
+    def __init__(self, strength=0.5):
+        super(PolarCoordinates, self).__init__()
+        self.strength = strength
+
+    def generate_warp_field(self, height, width):
+        y_indices, x_indices = torch.meshgrid(torch.linspace(-1, 1, height), torch.linspace(-1, 1, width))
+
+        r = torch.sqrt(x_indices ** 2 + y_indices ** 2)
+        theta = torch.atan2(y_indices, x_indices)
+
+        x_new = x_indices * torch.sin(theta)
+        y_new = y_indices * torch.cos(theta)
 
         x_new = x_new.clamp(-1, 1)
         y_new = y_new.clamp(-1, 1)

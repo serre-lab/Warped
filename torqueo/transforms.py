28b445ab11931e5dfa285d86d8db661890ae1f45
vipulSharma18 <vipuls181999@gmail.com>
2024-06-02 18:12:58 -0400

    pinch and punch done'

--- torqueo/transforms.py
+++ torqueo/transforms.py
@@ -278,3 +278,55 @@ class PolarCoordinates(WarpTransform):
         grid = torch.stack((x_new, y_new), dim=-1)
         grid = grid.unsqueeze(0)
         return grid
+
+class Pinch(WarpTransform):
+    def __init__(self, strength=0.5):
+        super(Pinch, self).__init__()
+        assert 0.0 <= strength <= 1.0
+        self.strength = strength
+
+    def generate_warp_field(self, height, width):
+        y_indices, x_indices = torch.meshgrid(torch.linspace(-1, 1, height), torch.linspace(-1, 1, width))
+
+        r = torch.sqrt(x_indices ** 2 + y_indices ** 2)
+
+        pinch_factor = torch.sin((3.14/2)* r) ** (-1*self.strength)
+        x_new = x_indices*pinch_factor
+        y_new = y_indices*pinch_factor
+
+        x_new = x_new.clamp(-1, 1)
+        y_new = y_new.clamp(-1, 1)
+
+        grid = torch.stack((x_new, y_new), dim=-1)
+        grid = grid.unsqueeze(0)
+        return grid
+
+class Punch(Pinch):
+    def __init__(self, strength=0.5):
+        super(Punch, self).__init__(-1*strength)
+
+class Mosaic(WarpTransform):
+    '''
+    Incomplete. 
+    Hexagonal or other tile shape with no color averaging in the tiles. 
+    The current code is using continuous warps, mosaic (discrete) isn't straighforward addition to it. 
+    '''
+    def __init__(self, strength=0.5):
+        super(Mosaic, self).__init__()
+        self.strength = strength
+
+    def generate_warp_field(self, height, width):
+        y_indices, x_indices = torch.meshgrid(torch.linspace(-1, 1, height), torch.linspace(-1, 1, width))
+
+        r = torch.sqrt(x_indices ** 2 + y_indices ** 2)
+        theta = torch.atan2(y_indices, x_indices)
+
+        x_new = x_indices 
+        y_new = y_indices
+
+        x_new = x_new.clamp(-1, 1)
+        y_new = y_new.clamp(-1, 1)
+
+        grid = torch.stack((x_new, y_new), dim=-1)
+        grid = grid.unsqueeze(0)
+        return grid

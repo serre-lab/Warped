485cbbfe493aaefffeed75022bc798fdec82a6c3
vipulSharma18 <vipuls181999@gmail.com>
2024-06-11 12:19:41 -0400

    Bump version: 0.0.1 → 0.0.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", encoding="utf-8") as fh:
 
 setup(
     name="Torqueo",
-    version="0.0.1",
+    version="0.0.2",
     description="Personal toolbox for image Warping",
     long_description=README,
     long_description_content_type="text/markdown",
